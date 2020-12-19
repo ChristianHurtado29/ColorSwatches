60e79e4931fe6d6c8e3b2e17d151563a5df3e2d9
Christian Hurtado <christian.hurtado.catacoli@gmail.com>
2020-12-19 00:47:29 -0500

    Initial Commit

---
+++ ColorSwatches/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ColorSwatches
+//
+//  Created by Christian Hurtado on 12/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let colorPicker = UIColorPickerViewController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addColor(_ sender: UIBarButtonItem) {
+        present(colorPicker, animated: true)
+    }
+    
+
+
+}
+
